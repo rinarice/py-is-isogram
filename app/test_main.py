73371794cd2635263155e7c09d3da4b4258c4a4d
Kateryna <kat.rin47u@gmail.com>
2024-05-24 22:49:11 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import is_isogram
+import pytest
+#  "Not only non-consecutive letters are not an isogram."
+# "Not only consecutive letters are not an isogram."
+# "Empty string is an isogram."
+# "String with different cases of the same letter is not an isogram."
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("aba", False),
+        ("a", True),
+        ("happy", False),
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
